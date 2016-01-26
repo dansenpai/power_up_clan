5034c589d9075a00b52bd2c6869b92071995f0bc
Daniel Santos <xtreme_daniel13@hotmail.com>
2016-01-26 12:00:06 -0300

    criado a página de regras e modificado algumas imagens

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get '/mix' => 'pages#mix'
   get '/sobre' => 'pages#sobre'
+  get '/regras' => 'pages#regras'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
